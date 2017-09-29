22c648a1076d9c0454ff6bee888d0d39562d4c90
Tana - HP <Tana - HP@Tana-HP>
2017-09-29 13:30:39 +0100

    Versión 1.3

---
+++ src/kata2/histogram.java
@@ -0,0 +1,23 @@
+
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class histogram {
+    private final int[] data;
+
+    public int[] getData() {
+        return data;
+    }
+    
+    public histogram(int[] data) {
+        this.data = data;
+    }
+    
+    public Map getHistogram(){
+        Map <Integer,Integer> histogram = new HashMap<>();
+        for (int value : data) histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
+        return histogram;
+    }
+}
