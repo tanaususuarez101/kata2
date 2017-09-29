da93ee1ab4f3df5672556350ed0f0137a0858de3
Tana - HP <Tana - HP@Tana-HP>
2017-09-29 13:18:35 +0100

    Versión 1.2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -8,16 +8,12 @@ public class Kata2 {
 
     public static void main(String[] args) {
         int[]data = {1,1,1,2,2,0,0,1,2,0,1,1,1,2,2,2};
-        Map <Integer,Integer> hist = new HashMap<>();
+        Map <Integer,Integer> histogram = new HashMap<>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(!hist.containsKey(data[i]))  hist.put(data[i], 1);            
-            else  hist.put(data[i], hist.get(data[i]) +1 );            
-            
-        }
-        System.out.println(hist.toString());
-        for(Object key : hist.keySet()){
-            System.out.println(key + " -> " + hist.get(key) + " veces");
+        
+        for (int value : data) histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
+        for(Object key : histogram.keySet()){
+            System.out.println(key + " -> " + histogram.get(key) + " veces");
         }
         
     }
