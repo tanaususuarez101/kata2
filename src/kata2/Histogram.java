220b105e9b7c2ea7090dd039c67070c40b806339
Tana - HP <Tana - HP@Tana-HP>
2017-09-29 13:37:23 +0100

    Versión 1.3

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -4,15 +4,21 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class histogram {
+public class Histogram {
+
+    static Iterable<Object> keySet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     private final int[] data;
 
     public int[] getData() {
         return data;
     }    
-    public histogram(int[] data) {
+    
+    public Histogram(int[] data) {
         this.data = data;
-    }    
+    }
+    
     public Map getHistogram(){
         Map <Integer,Integer> histogram = new HashMap<>();
         for (int value : data) histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -8,12 +8,12 @@ public class Kata2 {
 
     public static void main(String[] args) {
         int[]data = {1,1,1,2,2,0,0,1,2,0,1,1,1,2,2,2};
-        Map <Integer,Integer> histogram = new HashMap<>();
         
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
         
-        for (int value : data) histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
-        for(Object key : histogram.keySet()){
-            System.out.println(key + " -> " + histogram.get(key) + " veces");
+        for(Object key : histogr.keySet()){
+            System.out.println(key + " -> " + histogr.get(key) + " veces");
         }
         
     }
